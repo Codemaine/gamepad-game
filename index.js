a23256c747e659bec2624c5dbad3931f30614f6b
Jermaine <jermaineantwi22@gmail.com>
2021-06-26 14:27:35 +0000

    added back and forth movement

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 var gamepads = navigator.getGamepads();
+var player = document.getElementById('player');
+var left = 0
+var walker = [];
+            var curWalker = 0;
+            var startWalking;
+            for(var i=1; i<3; i++) {
+                walker[i] = new Image();
+                walker[i].src = "walk"+i+".png";
+            }
+
+            function marathon() {
+                if(curWalker == 5)
+                    curWalker = 0;
+                else
+                    ++curWalker;
+                let tag = document.getElementById("player")
+                tag.src = walker[curWalker].src;
+            }
+            function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 window.addEventListener("gamepadconnected", event => {
     console.log("gamepad connected");
     console.log(event.gamepad);
@@ -23,9 +45,24 @@ window.addEventListener("gamepadconnected", event => {
         }
         if(navigator.getGamepads()[0].axes[0] === -1.00000){
             console.log("left");
+            if(left <= 0){
+                left += 2;
+        }
+            player.src = "back2.png"
+            sleep(1).then(() => player.src = "back1.png")
+            left -=2
+            player.style.left = left + "px"
+
         }
         if(navigator.getGamepads()[0].axes[0] === 1.00000){
             console.log("right");
+            if(left >= 576){
+                left -= 2
+            }
+            player.src = "walk2.png"
+            sleep(1).then(() => player.src = "walk1.png")
+            left +=2
+            player.style.left = left + "px"
         }
         if(navigator.getGamepads()[0].buttons[5].pressed){
             console.log("jump");
@@ -53,6 +90,5 @@ window.addEventListener("gamepadconnected", event => {
         }
         requestAnimationFrame(update)
     }
-    
     update();
 })
